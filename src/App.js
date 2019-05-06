1639c09df97774a982eddf61d37b7dfeb8d622e4
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-05-06 17:28:41 +0530

    add useState function to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Row from 'Row'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//.............................. Function base Reactjs Hook...........................
+export default function App(props){
+  const [name , setName ] = useState('Trushant');
+  const [surname, setSurname ] = useState('Bhangre');
+
+  function handledChange(e){
+    setName(e.target.value);
+  }
+
+  function handledChangeSurname(e){
+    setSurname(e.target.value);
+  }
+  
+  return(
+    <form>
+      <label>name </label>
+      <input value={name}
+        onChange={handledChange}
+      />
+      <label>surname </label>
+      <input value={surname}
+        onChange={handledChangeSurname}
+      />
+    </form>
+  )
 }
 
-export default App;
+
+//............................ simple state method in class base...................................
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       name: "Trush",
+//       surname : "Bhangre"
+//     }
+//     // this.handledChange = this.handledChange.bind(this);
+//   }
+//   handledChange = (e)=>{
+//     this.setState({
+//       name: e.target.value
+//     })
+//   }
+
+//   handledChangeSurname = (e) =>{
+//     this.setState({
+//       surname:e.target.value
+//     })
+//   }
+// render(){
+//   return (
+//     <div className="App">
+//         <h1> Hook In Reactjs</h1>
+//       <form>
+//         <label>name </label>
+//         <input value={this.state.name}
+//           onChange={this.handledChange}
+//         />
+//         <label>surname name </label>
+//         <input value={this.state.surname}
+//           onChange={this.handledChangeSurname}
+//         />
+//       </form>
+        
+//     </div>
+//   );
+// }
+// }
+
+// export default App;
